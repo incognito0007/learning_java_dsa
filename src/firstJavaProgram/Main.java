5d8db06e6beed1dbd34fb7339fec7ba03d03bd7c
incognito007 <anandaankit480@gmail.com>
2025-05-18 05:24:08 +0530

    First Java Program and updated Readme file

---
+++ src/firstJavaProgram/Main.java
@@ -0,0 +1,8 @@
+package firstJavaProgram;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Ankit Anand";
+        System.out.println("Hello World, This is " + name);
+    }
+}
